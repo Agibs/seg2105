e87f78700a1647568dad6212f003b59526405c31
ralphbolo <rbolo062@uottawa.ca>
2014-11-14 17:38:34 -0500

    created update and show user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,32 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to root_url, :notice => "Signed up!"
+      auto_login(@user)
+  		redirect_to @user, :notice => "Signed up!"
   	else 
   		render :new
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user, :notice => "Profile updated"
+    else
+      render 'edit' 
+    end  
   end
 
   private
